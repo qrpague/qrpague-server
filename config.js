90778fcea73f9d94348cf93c55573f1bf33e4bee
leonardo macbook <support@crimatex.com>
2018-11-15 18:07:13 -0200

    change ports

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ var path = require('path');
 global.pathRootApp = path.resolve(__dirname);
 
 var configuracoes = {
-	httpPort : 9090,
+	httpPort : 9092,
 	httpHost : '0.0.0.0',
 	portWebsocket : 4000,
-	urlDataBase : 'localhost:27017'
+	urlDataBase : 'localhost:10001'
 };
 
 exports.Config = configuracoes;
