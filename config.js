9140f18bde08f7ce314fa2acfcc89d26da1de7e6
leonardo macbook <support@crimatex.com>
2018-11-25 14:43:32 -0200

    acertando porta mongodb

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var Config = {
 	HTTP_PORT : 9092,
 	HTTP_HOST : '0.0.0.0',
 	WEBSOCKET_PORT : 4000,
-	URL_DATABASE : 'localhost:27017'
+	URL_DATABASE : 'localhost:10001'
 };
 
 module.exports = Config;
