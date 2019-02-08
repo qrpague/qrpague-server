de159baad15f294244436349186832caace0602f
leonardoSilveira <leo.compiler@gmail.com>
2019-02-08 17:16:11 -0200

    acertando origin

--- security.js
+++ security.js
@@ -5,7 +5,7 @@ module.exports = {
     cors: function (req, res, next) {
         //Access-Control-Allow-Origin: *
 
-        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Origin", req.headers.origin );
         res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
         res.header("Access-Control-Allow-Credentials", true);
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
