d0e2c5b49e5982cbb19dea4275d63b6ec14761e8
leonardoSilveira <leo.compiler@gmail.com>
2019-02-08 17:27:08 -0200

    acertando cors

--- security.js
+++ security.js
@@ -3,15 +3,8 @@
 
 module.exports = {
     cors: function (req, res, next) {
-        //Access-Control-Allow-Origin: *
-
-        res.header("Access-Control-Allow-Origin", req.headers.origin );
-        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-        res.header("Access-Control-Allow-Credentials", true);
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        let ip = req.ip + " - " + req.connection.remoteAddress
-        console.debug(ip, req.path)
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         next();
     }
 
