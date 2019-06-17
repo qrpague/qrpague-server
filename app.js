b5e374b396dc286c7a1746ea1bb40844ce49339d
Emmanuel Kiametis <emmanuelkiametis@localhost.localdomain>
2019-06-17 12:01:50 -0300

    Incluindo versão de biblioteca compativel com OpenAPI 3

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const path = require('path');
 const setupConfiguration = require('./config/config');
 const Routes = require('./api/routes');
 const { Logger } = require('./api/util');
+const createMiddleware = require('swagger-express-middleware');
 
 const expressApp = express();
 
+const swaggerFilePath = path.join(__dirname, 'api/swagger/swagger.yaml');
 const PORT = process.env.PORT || 8080;
 
 const start = (app) => {
-    setupConfiguration(app);
-    app.use(Routes);
-    app.listen(PORT, () => Logger.info('O servidor do QR-PAGUE subiu na porta:', PORT));
+
+    createMiddleware(swaggerFilePath, app, function(err, middleware, api, parser) {
+    
+        setupConfiguration(app);
+
+        app.use(middleware.metadata())
+        app.use(middleware.CORS());
+        app.use(middleware.parseRequest());
+
+        app.use('/qrpague', Routes);
+        app.listen(PORT, () => Logger.info('O servidor do QR-PAGUE subiu na porta:', PORT));
+    });
 }
 
 
