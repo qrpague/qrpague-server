0ce6b8b606ed087c22b367a4aa1d9c970cc279d9
Emmanuel Kiametis <emmanuelkiametis@localhost.localdomain>
2019-10-15 12:18:16 -0300

    Aceitando campo 'valor' com valor = 0 (ZERO)

--- api/http-validators/commum.js
+++ api/http-validators/commum.js
@@ -1,7 +1,11 @@
 const { ResponseError, Err, Response } = require('../util');
 
 const campoObrigatorio = (nomeCampo, valor, options = { isRequest: true }) => {
-    if(typeof valor !== 'boolean' && !valor) {
+    const tipo = typeof valor;
+    if(tipo === 'number' && valor === 0) {
+        return;
+    }
+    if(tipo !== 'boolean' && !valor){
         if(options.isRequest) {
             Err.throwError(Response.HTTP_STATUS.BAD_REQUEST, 997000, 1, { campo: nomeCampo });
         } else {
